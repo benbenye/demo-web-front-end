c6cc9f94500fef15d5bb40b8263a75955f2a76af
benbenye <1203846281@qq.com>
2014-08-13 23:12:31 +0800

    rotate

--- demo-css/demo-css3-animate/leaves/leaves.js
+++ demo-css/demo-css3-animate/leaves/leaves.js
@@ -13,7 +13,7 @@ $(function () {
 			for(var i = 0; i < 30; ++i){
 				this.randomLeavesAry.push(Math.ceil(Math.random()*4));
 				this.randomLeftAry.push(Math.ceil(Math.random()*500));
-				this.randomTimeAry.push(Math.random()*2+3);
+				this.randomTimeAry.push(Math.random()*5+5);
 				this.randomDelayTimeAry.push(Math.random()*5);
 			}
 			console.log(this.randomLeavesAry);
@@ -25,7 +25,7 @@ $(function () {
 			this.randomFn();
 			var str = '';
 			for(var i = 0, l = this.randomLeavesAry.length; i < l; ++i){
-				str += '<div style="top:-100px;left:'+this.randomLeftAry[i]+'px;-webkit-animation:fade '+this.randomTimeAry[i]+'s '+this.randomDelayTimeAry[i]+'s infinite,dropDown '+this.randomTimeAry[i]+'s '+this.randomDelayTimeAry[i]+'s infinite;"><img src="images/realLeaf'+this.randomLeavesAry[i]+'.png" style="-webkit-animation:rotat '+this.randomTimeAry[i]+'s"></div>';
+				str += '<div style="top:-150px;left:'+this.randomLeftAry[i]+'px;-webkit-animation:fade '+this.randomTimeAry[i]+'s '+this.randomDelayTimeAry[i]+'s infinite,dropDown '+this.randomTimeAry[i]+'s '+this.randomDelayTimeAry[i]+'s infinite;"><img src="images/realLeaf'+this.randomLeavesAry[i]+'.png" style="-webkit-animation:rotat '+this.randomDelayTimeAry[i]+'s"></div>';
 			}
 			$('#containerleaves').append(str);
 		}
