dc2308ab4258188234e5f8ca248714d6bf201974
benbenye <1203846281@qq.com>
2014-08-13 19:01:06 +0800

    很多的

---
+++ demo-css/demo-css3-animate/leaves/leaves.js
@@ -0,0 +1,34 @@
+$(function () {
+	// containerleaves
+	// 先准备好 随机生成多少叶子-30
+	// 叶子有1-4随机填充
+	// 随机生成对应叶子的left定位
+	// 运动时间在9-10s之间
+	var fallLeaves = {
+		randomLeavesAry : [],
+		randomLeftAry : [],
+		randomTimeAry : [],
+		randomDelayTimeAry : [],
+		randomFn : function(){
+			for(var i = 0; i < 30; ++i){
+				this.randomLeavesAry.push(Math.ceil(Math.random()*4));
+				this.randomLeftAry.push(Math.ceil(Math.random()*500));
+				this.randomTimeAry.push(Math.random()*2+3);
+				this.randomDelayTimeAry.push(Math.random()*5);
+			}
+			console.log(this.randomLeavesAry);
+			console.log(this.randomLeftAry);
+			console.log(this.randomTimeAry);
+			console.log(this.randomDelayTimeAry);
+		},
+		toDom : function(){
+			this.randomFn();
+			var str = '';
+			for(var i = 0, l = this.randomLeavesAry.length; i < l; ++i){
+				str += '<div style="top:-100px;left:'+this.randomLeftAry[i]+'px;-webkit-animation:fade '+this.randomTimeAry[i]+'s '+this.randomDelayTimeAry[i]+'s infinite,dropDown '+this.randomTimeAry[i]+'s '+this.randomDelayTimeAry[i]+'s infinite;"><img src="images/realLeaf'+this.randomLeavesAry[i]+'.png" style="-webkit-animation:rotat '+this.randomTimeAry[i]+'s"></div>';
+			}
+			$('#containerleaves').append(str);
+		}
+	};
+	fallLeaves.toDom();
+});
