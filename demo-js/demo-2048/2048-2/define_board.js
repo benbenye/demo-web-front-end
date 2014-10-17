8af60b79fdd5d721d617d7017b706ce60863223f
benbenye <1203846281@qq.com>
2014-10-17 15:01:48 +0800

    2048 修复随机位置bug 完成上下左右方向的操作。雏形

--- demo-js/demo-2048/2048-2/define_board.js
+++ demo-js/demo-2048/2048-2/define_board.js
@@ -39,34 +39,39 @@ function Board(dimension,score,container){
 		if(empty){
 			var _ranCell = Math.random() > 0.5 ? {number : 2,color : 'c2', x : 0, y : 0} : {number : 4, color : 'c4', x : 0, y : 0},
 				_ranDes = 0;
-			// 生成随机出现的位置
-			_ranDes = Math.floor( Math.random() * empty + 1 );
 
-			// ？另一个方法，随机生成一个位置，按照一个规则顺序查找是否有一个空位置
+			// 随机生成一个位置，按照一个规则顺序查找是否有一个空位置
+			_ranDes = Math.floor( Math.random() * this.dimension * this.dimension );//0-15
+			_ranCell.x = parseInt( _ranDes / this.dimension );
+			_ranCell.y = _ranDes % this.dimension;
 
-			// 找到空位置坐标
-			for(var i = 0; i < dimension; ++i){//i为x轴
-				for( var j = 0; j < dimension; ++j){
-					if(!this.cells[i][j]){//如果是空
-						--_ranDes;
-					}
-					if(_ranDes == 0){//找到空位置
-						_ranCell.x = i;
-						_ranCell.y = j;
-						break;
-					}
-				}
-
-				if(_ranDes == 0){//找到空位置
+			for(var i = 0; i < this.dimension * this.dimension; ++i) {
+				if( !this.cells[_ranCell.x][_ranCell.y] ) {
+					// 实例化一个随机棋子
+					var cellNew = new Cell(_ranCell.x, _ranCell.y, _ranCell.color, _ranCell.number);
+					this.cells[_ranCell.x][_ranCell.y] = cellNew;
 					break;
-				}
+				}else{
+					if( _ranCell.y === (this.dimension - 1) ) {//本列最后一个
+						if( _ranCell.x === (this.dimension - 1) ) {//本行最后一个
+							_ranCell.x = 0;
+							_ranCell.y = 0;
+							continue;
+						} else {
+							_ranCell.x += 1;
+							_ranCell.y = 0;
+							continue;						
+						}
+					} else {					
+						_ranCell.y += 1;
+						continue;	
+					}
+				}				
 			}
 
-			// 实例化一个随机棋子
-			var cellNew = new Cell(_ranCell.x, _ranCell.y, _ranCell.color, _ranCell.number);
-
 			this.updateEmpty(cellNew);
-
+			console.log(cellNew);
+			console.log(_ranCell);
 			cellNew.show(this.container, cellNew);
 		}else{
 			alert('无空位置');
@@ -130,12 +135,109 @@ function Board(dimension,score,container){
 	*/
 	this.moveRight = function(){
 		console.log('d2');
+		// 从右向左横向遍历
+		var tempArray = [],//临时数组用于合并棋子
+			move = false,//是否移动棋子
+			merge = 0;//合并棋子对数
+
+		for(var i = 0; i < this.dimension; ++i){
+			for(var j = this.dimension - 1; j >= 0; --j){
+				if(this.cells[j][i]){
+					tempArray.push(this.cells[j][i]);
+					this.cells[j][i]= undefined;
+				}
+			}
+			console.log(tempArray);
+			console.log(this.cells);
+			if(tempArray.length>=1){
+				for(var m = 0; m < tempArray.length -1; ++m){
+					if(tempArray[m].number === tempArray[m+1].number){
+						tempArray[m].number = tempArray[m].number*2;
+						tempArray[m].color = 'c' + tempArray[m].number;
+						$('#grid-cell-'+tempArray[m+1].x+'-'+tempArray[m+1].y).detach();
+						tempArray.splice(m+1,1);
+						++merge;
+					}else{
+						continue;
+					}
+				}
+				for(var n = this.dimension - 1; n >= 0; --n){
+					this.cells[n][i] = tempArray[this.dimension - 1 - n];
+				}
+			}
+			console.log(this.cells);
+			//展示
+			for(var k = this.dimension - 1; k >= 0; --k){
+				if(this.cells[k][i]){
+					$('#grid-cell-'+this.cells[k][i].x+'-'+this.cells[k][i].y).detach();
+					if(!merge && this.cells[k][i].x != k) {
+						move = true;
+					console.log('merge:'+merge+'move:'+move+';i:'+i+';k:'+k+';x:'+this.cells[k][i].x+';y:'+this.cells[k][i].y );
+					}
+					this.cells[k][i].x=k;
+					this.cells[k][i].y=i;
+					ui.showCell(this.container, this.cells[k][i]);
+				}
+			}
+			tempArray=[];
+		}
+		this.updateEmpty(merge);
+		if(move || merge){
+			this.ranCell();
+		}
 	};
 	/*
 	*键盘下方向的操作
 	*/
 	this.moveDown = function(){
 		console.log('d3');
+		// 从上向下竖向遍历从左到右
+		var tempArray = [],//临时数组用于合并棋子
+			move = false,//是否移动棋子
+			merge = 0;//合并棋子对数
+
+		for(var i = 0; i < this.dimension; ++i){
+			for(var j = this.dimension - 1; j >= 0; --j){
+				if(this.cells[i][j]){
+					tempArray.push(this.cells[i][j]);
+					this.cells[i][j]= undefined;
+				}
+			}
+			if(tempArray.length >= 1){
+				for(var m = 0; m < tempArray.length -1; ++m){
+					if(tempArray[m].number === tempArray[m+1].number){
+						tempArray[m].number = tempArray[m].number*2;
+						tempArray[m].color = 'c' + tempArray[m].number;
+						$('#grid-cell-'+tempArray[m+1].x+'-'+tempArray[m+1].y).detach();
+						tempArray.splice(m+1,1);
+						++merge;
+					}
+				}
+				for(var n = this.dimension - 1; n >= 0; --n){
+					this.cells[i][n] = tempArray[this.dimension - 1 - n];
+				}
+			}
+			//展示
+			for(var k = this.dimension - 1; k >= 0; --k){
+				if(this.cells[i][k]){
+					$('#grid-cell-'+this.cells[i][k].x+'-'+this.cells[i][k].y).detach();
+					if(!merge && this.cells[i][k].y != k) {
+						move = true;
+					console.log('merge:'+merge+'move:'+move+';i:'+i+';k:'+k+';x:'+this.cells[i][k].x+';y:'+this.cells[i][k].y );
+					}
+					this.cells[i][k].x=i;
+					this.cells[i][k].y=k;
+					ui.showCell(this.container, this.cells[i][k]);
+				}
+			}
+			tempArray=[];
+		}
+		console.log(this.cells)	;
+		this.updateEmpty(merge);
+		if(move || merge){
+			console.log(move+''+merge);
+			this.ranCell();
+		}
 	};
 	/*
 	*键盘左方向的操作
